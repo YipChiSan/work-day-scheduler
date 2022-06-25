587b460d78e0dbf49688f2be3a31866ac5ba667f
YipChiSan <zye39@yahoo.com>
2022-06-25 15:43:11 +1000

    Finish the text area display element

--- asset/script.js
+++ asset/script.js
@@ -1,2 +1,39 @@
 var today = moment().format("ddd, MMM Do, YYYY");
 $("#currentDay").text(today);
+let containerEl = $(".container");
+
+let currentTime = parseInt(moment().format("k"));
+for (let i = 9; i <= 17; i++) {
+    let displayTime;
+    let textAreaTimeClass;
+    if (i > 12) {
+        displayTime = (i - 12) + "pm"; 
+    } else {
+        displayTime = i + "am";
+    }
+    
+    if (i < currentTime) {
+        textAreaTimeClass = "past";
+    } else if (i == currentTime) {
+        textAreaTimeClass = "present";
+    } else {
+        textAreaTimeClass = "future";
+    }
+
+    let displayTimeEl = $("<p class='col-1 time-block'>" + displayTime + "</p>");
+    
+    let textAreaEl = $("<textArea class='description " + textAreaTimeClass + "'></textArea>");
+    let bigColEl = $("<div class='col-10'></div>");
+    bigColEl.append(textAreaEl);
+
+    let btnEl = $("<button type='button' class='btn saveBtn'>Save</button>");
+    let btnColEl = $("<div class='col-1'></div>");
+    btnColEl.append(btnEl);
+
+    let rowEl = $("<div class='row no-gutters'></div>");
+    rowEl.append(displayTimeEl);
+    rowEl.append(bigColEl);
+    rowEl.append(btnColEl);
+
+    containerEl.append(rowEl);
+}
